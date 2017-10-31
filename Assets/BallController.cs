d6cdfec8b2e6dd58ad08d43030bd28f77434cd5f
Hayato matsuba <nkc.hayato.matsuba@gmail.com>
2017-10-31 09:42:46 +0900

    クリックしたら消える

---
+++ Assets/BallController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour
+{
+    GameObject sphere;
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(sphere);
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        this.sphere = GameObject.Find("SpherePrefab");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/BallGenerator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallGenerator : MonoBehaviour
+{
+    public GameObject BallPrefab;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject ball = Instantiate(BallPrefab) as GameObject;
+            //ball.GetComponent<BallController>().Shoot(new Vector3(0, 200, 2000));
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            ball.GetComponent<BallController>().Shoot(worldDir.normalized * 2000);
+        }
+	}
+}
